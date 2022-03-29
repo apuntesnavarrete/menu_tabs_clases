21646a0db70062fcdbf6f624237f23b6b2b803a3
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-29 02:18:53 -0600

    agregando recorido de arrays

--- index.js
+++ index.js
@@ -5,24 +5,11 @@ let opciones = document.querySelectorAll(".options")
 opciones.forEach((element , index)=> {
     element.addEventListener("click",()=>{
           console.log(element)
-                  //el que tenga el index es visible
-
-                    //el que no es no visible
-
-        bloques[0].classList.remove('novisible'); 
-        bloques[1].classList.remove('novisible'); 
-        bloques[2].classList.remove('novisible'); 
-        bloques[3].classList.remove('novisible');
-
-        bloques[0].classList.remove('visible'); 
-        bloques[1].classList.remove('visible'); 
-        bloques[2].classList.remove('visible'); 
-        bloques[3].classList.remove('visible');
-
+          console.log(bloques)
+                  bloques.forEach((bloque) => {
+                    bloque.classList.remove('novisible'); 
+                    bloque.classList.remove('visible'); 
+                  })
         bloques[index].classList.add('visible'); 
-
-        console.log(element)
-        console.log(index)
-
     })
 });
